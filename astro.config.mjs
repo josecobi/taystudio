ca350cf6798533137a3591443b5faed096fcbc43
josecobi <58313777+josecobi@users.noreply.github.com>
2024-09-12 11:23:29 -0400

    configure astro with static output for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'en',
